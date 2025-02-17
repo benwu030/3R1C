e279746eb2b1667ac9c5b05e1e34e7e3cea11917
Ben Wu <benwu013@connect.hku.hk>
2025-02-17 17:45:24 +0800

    add collection button

--- 3R1C/app/(root)/(tabs)/OutfitPlanning/OutfitCalendar.tsx
+++ 3R1C/app/(root)/(tabs)/OutfitPlanning/OutfitCalendar.tsx
@@ -1,40 +1,22 @@
-import React, { useEffect, useCallback, useState } from 'react'
-import { View, Text, FlatList, ActivityIndicator } from 'react-native'
 import { Image } from 'expo-image'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import {useLocalSearchParams, Link, router } from 'expo-router'
-import icons from '@/constants/icons'
-import ClothesCard from '@/components/ClothesCard'
-import Filters from '@/components/Filters'
-import { useAppwrite } from '@/lib/useAppWrite'
-import { getClothesWithFilter } from '@/lib/AppWrite'
-
+import { TouchableOpacity } from 'react-native'
 import Calendar from '@/components/Calendar'
+import icons from '@/constants/icons'
+import {router} from 'expo-router'
 
-import OutfitPreview from '@/components/OutfitPreview'
-const totalNumberClothes = "3"
+
 
 const OutfitCalendar = () => {
-  const params = useLocalSearchParams<{mainCategoryfilter?:string}>()
-  const {data:clothes,loading,refetch} = useAppwrite({fn:getClothesWithFilter})
-  const handleCardPressed = (id:string)=>{
-    console.log('card pressed',id)
-    router.push(`/details/${id}`)
-  }
 
-  
-  useEffect(()=>{
- 
-    refetch({mainCategoryfilter: params.mainCategoryfilter??'All'})
-
-  },[params.mainCategoryfilter])
-   
-  
   return (
 
-    <SafeAreaView className='bg-sand-dark h-screen'>
+    <SafeAreaView className='bg-sand-dark flex-1'>
+      <TouchableOpacity onPress={()=>router.push('/OutfitPlanning/Collections')} className='flex-row items-center p-2 '>
+        <Image source={icons.collectionsAdd} className='size-5'/>
+      </TouchableOpacity>
     
-        <Calendar />
+      <Calendar />
 
 
     </SafeAreaView>
